f881385c2f7b5647102707e34f70235e03b3a081
keymash <133459899+XQaij79DLCo3K1lYowiX@users.noreply.github.com>
2026-04-01 14:22:23 -0400

    Plugin Auto-Sync: zcServer/restoration.lua

---
+++ zcServer/restoration.lua
@@ -0,0 +1,19 @@
+wait(1)
+local chatted = _G.chatted
+local usersfolder = _G.usersFolder
+local Players = game:GetService("Players")
+
+
+
+Players.PlayerAdded:Connect(function(player)
+	player.Chatted:Connect(function(message)
+		chatted:Fire(message, player)
+	end)
+end)
+
+for _, player in ipairs(Players:GetPlayers()) do
+	player.Chatted:Connect(function(message)
+		chatted:Fire(message, player)
+	end)
+end
+
