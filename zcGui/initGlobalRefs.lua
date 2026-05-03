bbedb9fa8ac91daa4ab9228d3e04ca8eadcf418f
keymash <133459899+XQaij79DLCo3K1lYowiX@users.noreply.github.com>
2026-05-02 23:36:30 -0400

    changes to config fallback/loading, and added permission modes: zcGui/initGlobalRefs.lua

--- zcGui/initGlobalRefs.lua
+++ zcGui/initGlobalRefs.lua
@@ -1,9 +1,16 @@
--- same thing as the server side one, boring i know.
+-- make it so stuff doesnt have to have like a hardcoded location
+-- couldve done better, who cares
 
+-- if any contributers wanna change this to a better system, whatever, go ahead.
+_G.ep = game.ReplicatedStorage.zcRep.EntryPoint
 local refs = game.ReplicatedStorage.zcRep:GetChildren()
 
+
 for _, ref in ipairs(refs) do
+	if ref.Name == "EntryPoint" then
+		continue
+	end
 	_G[ref.Name] = ref
-	--print("[CLIENT] -- initilized _G." .. ref.Name)
+	--print("[SERVER] -- initilized _G." .. ref.Name)
 end
---print("[CLIENT] done initlizing")
+--print("[SERVER] done initlizing")
