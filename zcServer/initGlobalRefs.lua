332720e75c414e2ef41644f670079d6c77ee7a90
keymash <133459899+XQaij79DLCo3K1lYowiX@users.noreply.github.com>
2026-05-02 23:36:31 -0400

    changes to config fallback/loading, and added permission modes: zcServer/initGlobalRefs.lua

--- zcServer/initGlobalRefs.lua
+++ zcServer/initGlobalRefs.lua
@@ -2,10 +2,14 @@
 -- couldve done better, who cares
 
 -- if any contributers wanna change this to a better system, whatever, go ahead.
-
+_G.ep = game.ReplicatedStorage.zcRep.EntryPoint
 local refs = game.ReplicatedStorage.zcRep:GetChildren()
 
+
 for _, ref in ipairs(refs) do
+	if ref.Name == "EntryPoint" then
+		continue
+	end
 	_G[ref.Name] = ref
 	--print("[SERVER] -- initilized _G." .. ref.Name)
 end
